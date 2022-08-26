034fd68d647d926a98dcc81e7c12d4bb69df3106
yuzchen7 <yuzchen7@gmail.com>
2022-08-26 00:14:48 -0400

    [Features] Ignore spaces/substring when comparing

--- src/checker.cpp
+++ src/checker.cpp
@@ -10,7 +10,8 @@
 #include <fstream>
 #include <ctype.h> // using for ::isspace
 #include <algorithm> // using for std::remove_if()
-#define VERSION "v 1.3.0 Beta"
+#include <time.h>
+#define VERSION "v 1.4.0 Beta"
 #define AUTHOR "Yuz.Chen(Yuzhuang Chen)"
 
 /* if under windows system, will include <direct.h> from libray
@@ -33,33 +34,51 @@
 
 // open input file and check if input file is found
 void openInputFile(std::ifstream& inputFile, std::string& fileName);
+
 // remove all the space from the string
 void removeSpace(std::string& str);
+
+// remove the sub-string from a string if and only if subStr is exits in the string
+void removeSubString(std::string& str, const std::string& subStr);
+
 // close both input file stream
 void inputFileStreamClose(std::ifstream& expectedIN, std::ifstream& myoutputIN);
+
 // getting the driectory with assgin to the parameter resultPath
 void getCurrentDriectory(std::string& resultPath);
+
 // check if path starting with the home dri
 bool startWithHomeDriectory(const std::string& path);
+
 // print the man Manu
 void printMan();
+
 // print the ch tool version info
 void printVersion();
-// NOTE: Core code; to check two file contents if is the same or not
-int CoreChecker(int argc, char **argv);
+
+// print the ch tool usage line for tell the user command formatting
+int printUsage();
+
+// NOTE: Core code; to check two file contents if is the same
+int CoreChecker(int argc, char **argv, bool isIgnoreSpace, const std::string& subStringIgnore);
+
 
 int main(int argc, char *argv[]) {
-    if (argc <= 1) {
-        std::cout << "check: no file arguments" << std::endl;
-        return 0;
+    time_t start, end;
+    time(&start);
+
+    std::string lastArgument = std::string(argv[argc - 1]);
+    if (argc <= 1 || (argc >= 3 && lastArgument.find("-") != std::string::npos)) {
+        return printUsage();
     }
 
-    if (argc <= 2) {
-        int result = getopt(argc, argv, "-mv");
+    bool isIgnoreSpace = false;
+    int optResult = -1;
+    std::string subStringIgnore = "";
 
-        if (result != -1) {
-            switch (result)
-            {
+    if (argc <= 2) {
+        if ((optResult = getopt(argc, argv, "mv")) != -1) {
+            switch (optResult) {
             case 'm':
                 printMan();
                 break;
@@ -70,23 +89,43 @@ int main(int argc, char *argv[]) {
 
             case '?':
                 std::cout << "having Unidentifiable operation parameter." << std::endl;
-                break;
+                printUsage();
+                return 1;
             }
-        } else {
-            std::cout << "check: needs two file argument" << std::endl;
+            return 0;
         }
 
-        return 0;
+    } else if (argc >= 4) {
+        while ((optResult = getopt(argc, argv, "c:s")) != -1) {
+            switch (optResult) {
+            case 'c' :
+                subStringIgnore = optarg;
+                break;
+
+            case 's': 
+                isIgnoreSpace = !isIgnoreSpace;
+                break;
+
+            case '?':
+                std::cout << "having Unidentifiable operation parameter." << std::endl;
+                printUsage();
+                return 1;
+            }
+        }
     }
 
-    return CoreChecker(argc, argv);
+    int result = CoreChecker(argc, argv, isIgnoreSpace, subStringIgnore);
+
+    time(&end);
+    std::cout << "run with: " << difftime(end, start) << " seconds" << std::endl;
+
+    return result;
 }
 
-int CoreChecker(int argc, char **argv) {
+int CoreChecker(int argc, char **argv, bool isIgnoreSpace, const std::string& subStringIgnore) {
     std::string expectedINFile, myoutputINFile;
+
     expectedINFile = std::string(argv[argc - 2]);
-    myoutputINFile = std::string(argv[argc - 1]);
-    
     std::ifstream expectedIN;
     openInputFile(expectedIN, expectedINFile);
     if (!expectedIN) {
@@ -94,6 +133,7 @@ int CoreChecker(int argc, char **argv) {
         exit(-1);
     }
 
+    myoutputINFile = std::string(argv[argc - 1]);
     std::ifstream myoutputIN;
     openInputFile(myoutputIN, myoutputINFile);
     if (!myoutputIN) {
@@ -104,17 +144,28 @@ int CoreChecker(int argc, char **argv) {
     std::cout << "result is -> ";
 
     // prepar variable for loop 
-    std::string expectedinput, myoutputinput;
+    std::string expectedInput, myOutputInput;
     int counterLine = 1;
     // check each line in both file, to see if is correct
     while (!expectedIN.eof() && !myoutputIN.eof()) {
-        getline(expectedIN, expectedinput);
-        getline(myoutputIN, myoutputinput);
+        getline(expectedIN, expectedInput);
+        getline(myoutputIN, myOutputInput);
 
-        removeSpace(expectedinput);
-        removeSpace(myoutputinput);
+        if (isIgnoreSpace) {
+            removeSpace(expectedInput);
+            removeSpace(myOutputInput);
+        }
 
-        if (expectedinput != myoutputinput) {
+        if (!subStringIgnore.empty()) {
+            if (expectedInput.find(subStringIgnore) == myOutputInput.find(subStringIgnore)) {
+                continue;
+            } else {
+                removeSubString(expectedInput, subStringIgnore);
+                removeSubString(myOutputInput, subStringIgnore);
+            }
+        }
+
+        if (expectedInput != myOutputInput) {
             std::cout << "false : line " << std::to_string(counterLine) << std::endl;
             inputFileStreamClose(expectedIN, myoutputIN);
             return 0;
@@ -123,7 +174,7 @@ int CoreChecker(int argc, char **argv) {
         counterLine++;
     }
 
-    // if one of the file is not reach the end, then rest are error
+    // if one of the file is not reach the end, then the rest are error
     if (!expectedIN.eof() || !myoutputIN.eof()) {
         std::cout << "false : line : " << counterLine << " and keep on" << std::endl;
         inputFileStreamClose(expectedIN, myoutputIN);
@@ -135,7 +186,7 @@ int CoreChecker(int argc, char **argv) {
     return 0;
 }
 
-// open input file wiht modify path and check if input file is found
+// open input file and check if input file is found
 void openInputFile(std::ifstream& inputFile, std::string& fileName) {
     bool flagHomeDri = startWithHomeDriectory(fileName);
 
@@ -144,6 +195,7 @@ void openInputFile(std::ifstream& inputFile, std::string& fileName) {
     if (flagHomeDri) {
         fullPath = fileName;
     } else {
+        // Modify path: change the relative path to full path  with dynamically
         getCurrentDriectory(fullPath);
         if (std::to_string(fileName.at(0)) != PATH_SIGN) {
             fullPath.append(PATH_SIGN);
@@ -165,26 +217,40 @@ void removeSpace(std::string& str) {
     str.erase(std::remove_if(str.begin(), str.end(), ::isspace), str.end());
 }
 
+// remove the sub-string from a string if and only if subStr is exits in the string
+void removeSubString(std::string& str, const std::string& subStr) {
+    if (subStr.empty() || str.find(subStr) == std::string::npos) {
+        return;
+    }
+    
+    int index = 0;
+    while ((index = str.find(subStr)) != std::string::npos) {
+        str.erase(index, subStr.size());
+    }
+}
+
 // getting the driectory with assgin to the parameter resultPath
 void getCurrentDriectory(std::string& resultPath) {
     char result[PATH_MAX]; //PATH_MAX the max bytes for storage the path name
-    getcwd(result, sizeof(result)); //cwd = Current Working Dri
+    // getCurrentDri = getcwd / _getcwd @see #ifdef _WIN32
+    getCurrentDri(result, sizeof(result)); //cwd = Current Working Dri
     resultPath = std::string(result);
 }
 
-// check if path starting with HOME_PATH'~'
+// check if path starting with HOME_PATH '~'
 bool startWithHomeDriectory(const std::string& path) {
-    std::string homePath = getenv(HOME_PATH); // env = Environment Variables
+    std::string homePath = std::getenv(HOME_PATH); // env = Environment Variables
     if (path.find(homePath) != std::string::npos) {
         return true;
     }
     return false;
 }
 
+// print the man Manu
 void printMan() {
     std::cout << "NAME\n\tch - chcek file contents\n\n"
               << "SYNOPSIS\n"
-              << "\tch File_Path1 File_Path2\n"
+              << "\tch [-cs] source_file_1 source_file_2\n"
               << "\tch [-v | -m]\n\n"
               << "DESCRIPTION\n"
               << "\tIn the first form, the ch tool will check whether the contents of the two files, File_Path1 and File_Path2, are the same. The default comparison process excludes all spaces that appear in the content, but not the newline character '\\n'.\n\n"
@@ -193,6 +259,8 @@ void printMan() {
               << "\tThe following options are available:\n\n"
               << "\t-v\tDisplay the version of ch command\n\n"
               << "\t-m\tDisplay the help manu of the command; more info, @see README\n\n" 
+              << "\t-s\tCompare the two file without(Ignore) the space\n\n"
+              << "\t-c\tCompare the two file without(Ignore) the Substring\n\n" 
               << "\tIt is an error occurs if the target file or directory does not exist, and an error message is output.\n\n"
               << "EXIT STATUS\n"
               << "\tThe command \"mv File_Path1 File_Path2\" will be output with an error message.\n"
@@ -203,8 +271,17 @@ void printMan() {
               << std::endl; 
 }
 
+// print the ch tool version info
 void printVersion() {
     std::cout << "ch " << VERSION << "\n\n" 
               << "Author: " << AUTHOR 
               << std::endl;
+}
+
+// print the ch tool usage line for tell the user command formatting
+int printUsage() {
+    std::cout << "usage: ch [-cs] source_file_1 source_file_2\n"
+                 "       ch [-m | -v]"
+              << std::endl;
+    return -1;
 }
